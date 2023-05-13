58c95fae491fd98a1389230e94651a1bf41a1b94
yagmurtuncel <yagmurtuncell@hotmail.com>
2023-05-13 14:39:08 +0300

    Ladder

--- Assets/Scripts/LadderMovement.cs
+++ Assets/Scripts/LadderMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class LadderMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float vertical;
+    private float speed = 8f;
+    bool isLadder;
+    bool isClimbing;
+
+    [SerializeField] Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        vertical = Input.GetAxis("Vertical");
+
+        if(isLadder && Mathf.Abs(vertical) > 0f) 
+        { 
+            isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 6f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
     }
 }
